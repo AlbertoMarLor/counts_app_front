1986684cf162ffdfc10d1201f0eb71194c1f8c48
AlbertoMarLor <amartinl92@gmail.com>
2023-04-18 13:29:12 +0200

    formularios de registro y login terminados

--- src/app/components/users/login/login.component.ts
+++ src/app/components/users/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'login',
@@ -6,5 +9,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+  formulario: FormGroup
+  password: string;
+
+  constructor(private userService: UsersService,
+    private router: Router) {
+
+    this.password = 'password'
+    this.formulario = new FormGroup({
+
+      email: new FormControl(null, [
+        Validators.required,
+        Validators.pattern(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/)
+      ]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,8}$/)
+      ]),
+
+    });
+  }
+
+  async onSubmit() {
+    await this.userService.login(this.formulario.value);
+    this.router.navigate(['/groups']);
+  }
+
+
+  checkError(control: string, validator: string) {
+    return this.formulario.get(control)?.hasError(validator) && this.formulario.get(control)?.touched;
+  }
+
+
+  showPassword() {
+    this.password = this.password === 'text' ? 'password' : 'text';
+  }
 
 }
