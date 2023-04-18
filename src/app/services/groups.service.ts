544c1ba97a9c81259312454de9d764c0cd69ae52
AlbertoMarLor <amartinl92@gmail.com>
2023-04-18 17:35:30 +0200

    groups.service en proceso

--- src/app/services/groups.service.ts
+++ src/app/services/groups.service.ts
@@ -1,15 +1,55 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GroupsService {
+  private baseUrl: string;
 
-  constructor() {
+
+  constructor(private httpClient: HttpClient) {
+
+    this.baseUrl = 'http://localhost:3000/api/groups'
+
+  }
+
+  getAll() {
+    return firstValueFrom(
+      this.httpClient.get<any[]>(`${this.baseUrl}`)
+    )
+  }
+
+  getById(groupId: number) {
+    return firstValueFrom(
+      this.httpClient.get<any[]>(`${this.baseUrl}/:${groupId}`)
+    )
   }
 
   create(body: any) {
-
+    return firstValueFrom(
+      this.httpClient.post<any[]>(`${this.baseUrl}/newGroup`, body)
+    )
   }
+
+  addUser(body: any, username: string, userId: number) {
+    return firstValueFrom(
+      this.httpClient.post<any[]>(`${this.baseUrl}:/${userId}/adduser/:${username}`, body)
+    )
+  }
+
+  update(body: any, groupId: number) {
+    return firstValueFrom(
+      this.httpClient.post<any[]>(`${this.baseUrl}/edit/:${groupId}`, body)
+    )
+  }
+
+  delete(groupId: number) {
+    return firstValueFrom(
+      this.httpClient.delete<any[]>(`${this.baseUrl}/:${groupId}`)
+    )
+  }
+
 
 }
